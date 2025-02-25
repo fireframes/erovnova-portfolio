d39a3dadb85ad257c2ebbf585544e2d7f41ae31d
fireframes <maximejemer@gmail.com>
2025-02-26 06:48:26 +0700

    fixed space in footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,7 @@ const Footer = () => {
   return (
     <footer>
       <p>
-        &copy; 2025 Елизавета Ровнова,
-        <br className="mobile-break" />
+        &copy; 2025 Елизавета Ровнова, <br className="mobile-break" />
         психолог-консультант секс-терапевт
       </p>
     </footer>
