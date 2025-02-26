1a01da0a854277257454b90312ecde189ed85367
fireframes <maximejemer@gmail.com>
2025-02-26 23:25:04 +0700

    several big fixes started

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,42 +1,45 @@
 import React from "react";
 import { Mail, Phone, MapPin, Calendar, Clock } from "lucide-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTelegram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { telegramLink, whatsupLink } from "../constants";
 
 const Contacts = () => {
   return (
     <section id="contacts">
       <div className="container">
         <h2>Контакты</h2>
-        <div className="contacts-grid">
-          {/* Contact Information */}
-          <div className="contact-card">
-            <h3>Связаться со мной</h3>
-            <div className="contact-item">
-              <Mail className="icon" />
-              <a href="mailto:your.email@example.com">your.email@example.com</a>
-            </div>
-            <div className="contact-item">
-              <Phone className="icon" />
-              <a href="tel:+1234567890">+7 (XXX) XXX-XX-XX</a>
-            </div>
-            <div className="social-links">
-              {/* Add your social media links */}
-              <p>Telegram / WhatsApp</p>
-            </div>
+        <div className="contact-card">
+          <h3>Связаться со мной</h3>
+          <div className="contact-item">
+            <Mail className="icon" />
+            <a href="mailto:elizaveta.rovnova@gmail.com">
+              elizaveta.rovnova@gmail.com
+            </a>
           </div>
-
-          {/* Contact Form or Additional Info */}
-          {/* <div className="contact-card">
-            <h3>Записаться на консультацию</h3>
-            <p>Чтобы записаться на консультацию:</p>
-            <ol>
-              <li>Напишите мне на почту или в мессенджеры</li>
-              <li>Укажите удобное для вас время</li>
-              <li>Кратко опишите причину обращения</li>
-            </ol>
-            <p className="response-time">
-              Я отвечаю на сообщения в течение 24 часов
-            </p>
-          </div> */}
+          <div className="contact-item">
+            <Phone className="icon" />
+            <a href="tel:+79031812421">+7 (903) 181-24-21</a>
+          </div>
+          <div className="social-links">
+            <p>Telegram / WhatsApp</p>
+            <a
+              className="tg"
+              href={telegramLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faTelegram} size="2xl" />
+            </a>
+            <a
+              className="wu"
+              href={whatsupLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faWhatsapp} size="2xl" />
+            </a>
+          </div>
         </div>
       </div>
     </section>
--- src/components/Prices.tsx
+++ src/components/Prices.tsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react";
 import { telegramLink } from "../constants";
+import axios from "axios";
 
 const Prices = () => {
+  // const [rateEur, setRateEur] = useState(0);
+  const rateEur = 0.0109956821;
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         "https://api.currencyapi.com/v3/latest?apikey=cur_live_MvXRWO7dizhUSYp2Tv0hVFoxmI1qBn9nK79QZsJ8&currencies=EUR%2CUSD&base_currency=RUB"
+  //       );
+
+  //       // const today = new Date().getDay();
+  //       // if (lastFetchedDay === today)
+
+  //       setRateEur(response.data.data.EUR.value);
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
+
+  // console.log(rateEur);
+
+  const payRub = 2000;
+
+  const convertEur = Math.round(payRub * rateEur);
+
   return (
     <section id="prices">
       <div className="container">
@@ -8,7 +36,7 @@ const Prices = () => {
         <div className="card">
           <h3>Индивидуальная консультация</h3>
           <p>
-            <strong>2000₽</strong> или <strong>20€</strong> / 60 мин
+            <strong>2000₽</strong> или <strong>{convertEur}€</strong> / 60 мин
           </p>
           <a
             href={telegramLink}
